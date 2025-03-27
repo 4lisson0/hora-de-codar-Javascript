9ecce80ecf262bf6f529d8c14c7099530451fdbe
Alisson Sartori <alissonsartori33@gmail.com>
2025-03-27 15:51:47 -0300

    fix hora de codar 3

--- Hora de Codar 3 - De volta ao Código/exercicio 11.js
+++ Hora de Codar 3 - De volta ao Código/exercicio 11.js
@@ -1,16 +1,20 @@
-//11 - Escreva um programa para imprimir todas as tabuadas de 1 a N. N será informado pelo usuário.
+// 11 - Escreva um programa para imprimir todas as tabuadas de 1 a N. N será informado pelo usuário.
 
 function inicio() {
   var numTabuada, x, y;
 
-  x = parseInt(prompt("Digite o valor de N: "));
+  numTabuada = parseInt(prompt("Digite o valor de N: "));
 
+  if (numTabuada <= 0) {
+    document.write("<p>Por favor, insira um número válido maior que 0.</p>");
+    return;
+  }
+
+  document.write("<h1>Tabuadas de 1 até " + numTabuada + "</h1>");
   for (x = 1; x <= numTabuada; x++) {
-    alert("\nTabuada do " + x + ":\n");
-
+    document.write("<h2>Tabuada do " + x + ":</h2>");
     for (y = 1; y <= 10; y++) {
-      alert(x + " * " + y + " = " + x * y + "\n");
+      document.write("<p>" + x + " x " + y + " = " + (x * y) + "</p>");
     }
   }
 }
-inicio();
